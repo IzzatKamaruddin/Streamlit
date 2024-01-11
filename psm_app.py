df11a8e237bb976e3d29560b88b554657c66f5f3
IzzatKamaruddin <73515475+IzzatKamaruddin@users.noreply.github.com>
2024-01-11 14:05:59 +0800

    Update psm_app.py

--- psm_app.py
+++ psm_app.py
@@ -1,21 +1,6 @@
-import numpy as np
-import pandas as pd
-import streamlit as st
-import pickle
+st.sidebar.header('User Input Parameters')
 
-# Load the trained model using pickle
-model = pickle.load(open('model.sav', 'rb'))
-
-def predict_sp(features):
-    features = np.array(features)
-    prediction = model.predict(features)
-    probability = model.predict_proba(features)[0][1]
-    return prediction, probability
-
-def main():
-    st.title("Virus Prediction Model")
-    st.write("Please select the features values to predict the virus species")  # Fixed the string syntax
-
+def user_input_features():
     Nucleus = st.sidebar.slider('Nucleus', 0.0, 1.0, 0.5)
     Exosome = st.sidebar.slider('Exosome', 0.0, 1.0, 0.5)
     Ribosome = st.sidebar.slider('Ribosome', 0.0, 1.0, 0.5)
@@ -33,20 +18,9 @@ def main():
     }
 
     features = pd.DataFrame(data, index=[0])
+    return features
 
-    st.subheader('User Input:')
-    st.write(features)  # Use the correct variable 'features' instead of 'user_input'
+user_input = user_input_features()
 
-    if st.button("Predict Species"):
-        # Convert user input features to a list
-        features_list = features.values.tolist()[0]
-
-        # Predict species and probability
-        prediction, probability = predict_sp(features_list)
-
-        # Display the results
-        st.write(f"Prediction: {prediction[0]}")
-        st.write(f"Probability: {probability}")
-
-if __name__ == "__main__":
-    main()
+st.subheader('User Input:')
+st.write(user_input)
