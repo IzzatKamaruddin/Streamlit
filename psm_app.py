64fad664f50ef09fbc0da7d0cf5d5a91f23fc907
IzzatKamaruddin <73515475+IzzatKamaruddin@users.noreply.github.com>
2024-01-12 15:29:40 +0800

    Update psm_app.py

--- psm_app.py
+++ psm_app.py
@@ -2,7 +2,7 @@ import numpy as np
 import pickle
 import streamlit as st
 
-loaded_model = pickle.load(open('trained_model.sav', 'rb'))
+loaded_model = pickle.load(open('model.sav', 'rb'))
 
 
 def predict_NB(input_data):
