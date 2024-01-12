67656b0d0db8e8d12741149a16c6cde503da219b
IzzatKamaruddin <73515475+IzzatKamaruddin@users.noreply.github.com>
2024-01-12 15:01:14 +0800

    Update psm_app.py

--- psm_app.py
+++ psm_app.py
@@ -1,13 +1,27 @@
 import numpy as np
-import pandas as pd
+import pickle
 import streamlit as st
-from model import load_model, predict_species
+loaded_model = pickle.load(open('trained_model.sav','rb'))
 
-model = load_model('model.sav')
+
+def predict_NB(input_data):
+    
+
+    # changing the input_data to numpy array
+    input_data_as_numpy_array = np.asarray(input_data)
+
+    # reshape the array as we are predicting for one instance
+    input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+
+    prediction = loaded_model.predict(input_data_reshaped)
+    print(prediction)
+
+    return('The prediction result from your input is: ', NB_prediction)
+
 
 st.sidebar.header('User Input Parameters')
 
-def user_input_features():
+def main():
     Nucleus = st.sidebar.slider('Nucleus', 0.0, 1.0, 0.5)
     Exosome = st.sidebar.slider('Exosome', 0.0, 1.0, 0.5)
     Ribosome = st.sidebar.slider('Ribosome', 0.0, 1.0, 0.5)
@@ -15,31 +29,23 @@ def user_input_features():
     Endoplasmic_Reticulum = st.sidebar.slider('Endoplasmic Reticulum', 0.0, 1.0, 0.5)
     Cytosol = st.sidebar.slider('Cytosol', 0.0, 1.0, 0.5)
 
-    data = {
+    data = [
         'Nucleus': Nucleus,
         'Exosome': Exosome,
         'Ribosome': Ribosome,
         'Membrane': Membrane,
         'Endoplasmic_Reticulum': Endoplasmic_Reticulum,
         'Cytosol': Cytosol
-    }
-
-    features = pd.DataFrame(data, index=[0])
-    return features
-
-user_input = user_input_features()
-
-st.subheader('User Input:')
-st.write(user_input)
+    ]
 
 
-# Make prediction
-prediction, probability = predict_species(model, user_input.values)
+   diagnosis = ''
+    if st.button('Predict!'):
+        diagnosis = predict_NB(data)
+        
+    st.success(diagnosis)    
+    
 
-# Display the prediction and probability
-st.subheader('Prediction:')
-st.write(prediction)
 
-st.subheader('Probability:')
-st.write(probability)
-
+if __name__ == '__main__':
+     main()    
